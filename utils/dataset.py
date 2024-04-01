c31b90773bdde005eeaf03b885a660eef7e27ff4
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2024-04-01 20:43:31 +0800

    class attributes formally renamed

--- utils/dataset.py
+++ utils/dataset.py
@@ -6,25 +6,25 @@ from typing import List
 
 class FLClientDataset():
     def __init__(self, dataset: Dataset, num_clients: int, num_sample_per_client: int, non_iid_ratio: int, unique_sample_sharing: bool=True, *args, **kwargs) -> None:
-        self.num_clients = num_clients
-        self.num_sample_per_client = num_sample_per_client
-        self.non_iid_ratio = non_iid_ratio
-        self.unique_sample_sharing = unique_sample_sharing
+        self._num_clients_ = num_clients
+        self._num_sample_per_client_ = num_sample_per_client
+        self._non_iid_ratio_ = non_iid_ratio
+        self._unique_sample_sharing_ = unique_sample_sharing
 
-        self.dataset = dataset
-        self.args = args
-        self.kwargs = kwargs
+        self._dataset_ = dataset
+        self._args_ = args
+        self._kwargs_ = kwargs
 
-        self.__client_sample_id__ = [[] for _ in range(self.num_clients)]
-        self.__share_data__(self.kwargs['shuffle'], self.kwargs['random_state'])
+        self.__client_sample_id__ = [[] for _ in range(self._num_clients_)]
+        self.__share_data__(self._kwargs_['shuffle'], self._kwargs_['random_state'])
 
 
     def __share_data__(self, shuffle: bool=True, random_state: int=215):
-        if self.unique_sample_sharing and self.num_clients * self.num_sample_per_client > len(self.dataset):
+        if self._unique_sample_sharing_ and self._num_clients_ * self._num_sample_per_client_ > len(self._dataset_):
             raise IndexError("No enough samples for splitting")
         
         indices_by_class = defaultdict(list)
-        for idx, (_, y) in enumerate(self.dataset):
+        for idx, (_, y) in enumerate(self._dataset_):
             indices_by_class[y].append(idx)
         num_classes = len(indices_by_class)
 
@@ -33,12 +33,12 @@ class FLClientDataset():
             for class_id in indices_by_class.keys():
                 random.shuffle(indices_by_class[class_id])            
         
-        main_class_number = int(self.num_sample_per_client * self.non_iid_ratio)
-        other_class_number = (self.num_sample_per_client - main_class_number) // (num_classes - 1)
+        main_class_number = int(self._num_sample_per_client_ * self._non_iid_ratio_)
+        other_class_number = (self._num_sample_per_client_ - main_class_number) // (num_classes - 1)
 
-        if self.unique_sample_sharing:
+        if self._unique_sample_sharing_:
             p = [0 for _ in range(num_classes)]
-            for client_id in range(self.num_clients):
+            for client_id in range(self._num_clients_):
                 for class_id in indices_by_class.keys():
                     num_data = main_class_number if client_id % num_classes == class_id else other_class_number
                     self.__client_sample_id__[client_id].extend(indices_by_class[class_id][p[class_id] : p[class_id] + num_data])
@@ -46,7 +46,7 @@ class FLClientDataset():
         
         else:
             try:
-                for client_id in range(self.num_clients):
+                for client_id in range(self._num_clients_):
                     for class_id in indices_by_class.keys():
                         num_data = main_class_number if client_id % num_classes == class_id else other_class_number
                         self.__client_sample_id__[client_id].extend(random.choice(indices_by_class[class_id], num_data, replace=False).tolist())
@@ -55,7 +55,7 @@ class FLClientDataset():
             
 
     def getClientDataset(self, idx: int) -> Subset:
-        return Subset(self.dataset, self.__client_sample_id__[idx])
+        return Subset(self._dataset_, self.__client_sample_id__[idx])
     
     
     def getClientSampleId(self, idx: int) -> List[int]:
